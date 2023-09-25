3afb3a6800b8e0484dc86c77cd9d3aae1577e1c3
amlanstudio <sarah.ngotta@yahoo.fr>
2023-09-25 15:10:38 +0200

    scene2

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ import * as THREE from 'three';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({antialias : true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+$('body').append(renderer.domElement);
+
+var cube= new THREE.BoxGeometry(1,1,0.5);
+var material = new THREE.MeshPhongMaterial({color:salmon});
+var ground = new THREE.Mesh(cube,material);
+scene.add(ground);
+
+ground.position.z=-5;
+ground.rotation.x=2;
+ground.rotation.y=6;
+
+renderer.render(scene,camera);
+
