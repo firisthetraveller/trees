77135d4318eec767ca345889c9de476128c61d4c
amlanstudio <sarah.ngotta@yahoo.fr>
2023-09-25 15:03:29 +0200

    scene

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import * as THREE from 'three';
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
+
+var renderer = new THREE.WebGLRenderer();
