6e168bc30e903234a5bb8ba5658fde11aeff8971
Guy Luong <guy.luong.7@gmail.com>
2023-09-27 20:41:51 +0200

    Add: Linear megatrees

--- main.js
+++ main.js
@@ -70,7 +70,95 @@ treeRotationFolder.add(tree.rotation, 'z', 0, Math.PI * 2);
 //     }),
 // ];
 
-renderer.render(scene,camera);
+// L-System
+class Rule {
+	/**
+	 * 
+	 * @param {str} axiom 
+	 * @param {dict(str, str)} rules
+	 */
+	constructor(axiom, rules) {
+		this.axiom = axiom;
+		this.rules = rules;
+	}
+
+	/**
+	 * 
+	 * @param {int} depth
+	 * @param {string} s
+	 * @returns {string} a string generated from the axiom and following the rules
+	 */
+	generate(depth, s = this.axiom) {
+		if (depth == 0) {
+			return s;
+		}
+
+		let nextString = '';
+		for (let i = 0; i < s.length; i++) {
+			nextString += this.rules[s.charAt(i)];
+		}
+		return this.generate(depth - 1, nextString);
+	}
+}
+
+let basicRule = new Rule(
+	'A',
+	{ 'A': 'AABA', 'B': 'B', '[': '[', ']': ']' });
+
+const offsetValue = 20;
+const height = 1;
+
+/**
+ * 
+ * @param {THREE.Object3D} tree
+ * @param {THREE.Vector3} position the position of the base
+ * @param {string} lstring generated from a rule
+ * @returns a tree as a THREE.Object3D
+ */
+function createTree(tree, position, lstring, radius = 0.3, angle = 0) {
+	if (lstring.length == 0) {
+		return tree;
+	}
+
+	let geometry;
+	let material;
+	let mesh;
+	let computedRadius;
+
+	console.log(lstring);
+	console.log('Position: ');
+	console.log(position);
+
+	switch (lstring.charAt(0)) {
+	case '[':
+		return createTree(tree, position, lstring.slice(1), radius, angle + offsetValue);
+	case ']':
+		return createTree(tree, position, lstring.slice(1), radius, angle);
+	case 'A':
+		console.log('Creating a trunk');
+		computedRadius = Math.max(radius - 0.03, 0.1);
+		geometry = new THREE.CylinderGeometry(computedRadius, radius, height, 20, 32);
+		material = new THREE.MeshPhongMaterial({ color: 0xffaa00 });
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.copy(position);
+		tree.add(mesh);
+
+		return createTree(tree, new THREE.Vector3(0, height, 0).add(position), lstring.slice(1), computedRadius, angle);
+	case 'B':
+		console.log('Creating a fruit');
+		geometry = new THREE.SphereGeometry(0.2, 8, 20, 32);
+		material = new THREE.MeshPhongMaterial({ color: 0xff00 });
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.copy(position).add(new THREE.Vector3(radius - 0.1, 0, 0));
+		tree.add(mesh);
+		return createTree(tree, position, lstring.slice(1), radius, angle);
+	}
+}
+
+tree = createTree(tree, new THREE.Vector3(0, 0, 0), basicRule.generate(3));
+tree.position.set(0, -0.5, 0);
+scene.add(tree);
+
 // Create an animation loop
 const animate = () => {
 	renderer.render(scene, camera);
